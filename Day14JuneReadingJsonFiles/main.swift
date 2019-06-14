2c09b5c7a472c85820fe6ae178a38e9938dd0f37
Gurwinder Singh <singhg1696>
2019-06-14 18:15:05 -0400

    Initial Commit

---
+++ Day14JuneReadingJsonFiles/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day14JuneReadingJsonFiles
+//
+//  Created by MacStudent on 2019-06-14.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
